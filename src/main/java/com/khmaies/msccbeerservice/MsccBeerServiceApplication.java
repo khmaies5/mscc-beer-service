c0fa80a331350a5e7ad0f1c62968889c5e5992ca
Hassen Khmaies <khmaieshassen@hotmail.com>
2024-03-04 15:50:30 +0100

    adding files and fixing. Closes #1

---
+++ src/main/java/com/khmaies/msccbeerservice/MsccBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.khmaies.msccbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsccBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsccBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khmaies/msccbeerservice/MsccBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khmaies.msccbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsccBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
